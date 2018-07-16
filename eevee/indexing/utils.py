1bdaf10e552c5970bff4e88c0d29d01f11ef22df
Josh Humphries <j.humphries@nhm.ac.uk>
2018-07-16 19:55:34 +0100

    Add function for recombining data at a certain version

--- eevee/indexing/utils.py
+++ eevee/indexing/utils.py
@@ -1,6 +1,8 @@
 import json
 from datetime import datetime
 
+import dictdiffer
+
 
 class IndexData:
     """
@@ -46,3 +48,30 @@ def serialise_for_elasticsearch(data):
     :return: a json string
     """
     return json.dumps(data, default=non_standard_type_converter)
+
+
+def get_data_at_version(mongo_doc, target_version):
+    """
+    Convenience utility function which will return the data from the passed mongo_doc as it looked at the given version.
+    If the version passed is newer than the versions available then the latest version is returned. If the version is
+    older than the first version of the data then an empty dictionary is returned.
+
+    :param mongo_doc: the document from mongo
+    :param target_version: the version we want, this should be a timestamp in seconds from the UNIX epoch
+    :return: a dictionary of the data at the given target version
+    """
+    # this variable will hold the actual data of the record and will be updated with the diffs as we
+    # go through them. It is important, therefore, that it starts off as an empty dict because this
+    # is the starting point assumed by the ingestion code when creating a records first diff
+    data = {}
+    # iterate over the versions in pairs. The second part of the final pair will always be None to
+    # indicate there is no "next_version" as the "version" is the current one
+    for version in mongo_doc['versions']:
+        # sanity check
+        if version in mongo_doc['diffs']:
+            # compare the versions in int form otherwise lexical ordering will be used
+            if int(target_version) < int(version):
+                return data
+            # update the data dict with the diff
+            dictdiffer.patch(mongo_doc['diffs'][version], data, in_place=True)
+    return data
