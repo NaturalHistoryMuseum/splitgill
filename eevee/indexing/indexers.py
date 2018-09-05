676d67de4ad5ceaa74de9930b229c618438a43af
Josh Humphries <j.humphries@nhm.ac.uk>
2018-09-05 14:19:32 +0100

    Bulk write to Elasticsearch in a thread
    This commit alters how we push the documents to index to elasticsearch by
    sending them in a thread on the end of a queue. Doing this works from a
    performance point of view as the GIL is given up when the socket writes
    the data to the elasticsearch server allowing other threads to continue,
    i.e. the main thread that is processing the mongo documents.

--- eevee/indexing/indexers.py
+++ eevee/indexing/indexers.py
@@ -3,41 +3,50 @@
 import itertools
 from collections import Counter, defaultdict
 from datetime import datetime
+from queue import Queue
+from threading import Thread
 
 from eevee.indexing.utils import DOC_TYPE, get_elasticsearch_client
 from eevee.mongo import get_mongo
-from eevee.utils import OpBuffer
+from eevee.utils import chunk_iterator
 from eevee.versioning import Versioned
 
 
-class BulkIndexOpBuffer(OpBuffer):
+class ElasticsearchBulkWriterThread(Thread):
     """
-    OpBuffer implementation which sends bulk index operations to elasticsearch.
+    Thread which iterates over a queue of elasticsearch indexing commands, sending them off to elasticsearch in batches.
     """
 
-    def __init__(self, config, size, elasticsearch):
+    def __init__(self, elasticsearch, queue, bulk_size, *args, **kwargs):
         """
-        :param config: the config object
-        :param size: the maximum size the buffer can reach before it is handled and flushed, defaults to 1000
+        :param indexes: the index objects we're indexing into
+        :param elasticsearch: the elasticsearch client object to use
+        :param queue: the queue object to take the commands from
+        :param bulk_size: how many commands to send to elasticsearch in one request
+        :param args: Thread.__init__ args
+        :param kwargs: Thread.__init__ kwargs
         """
-        super().__init__(size)
-        self.config = config
+        super().__init__(*args, **kwargs)
         self.elasticsearch = elasticsearch
-        # this is for keeping track of what we index
+        self.queue = queue
+        self.bulk_size = bulk_size
+        # store the statistics about the indexing operations in this attribute
         self.stats = defaultdict(Counter)
 
-    def handle_ops(self):
+    def run(self):
         """
-        Handles the ops in the buffer by passing them to the elasticsearch module's send_bulk_index function. The stats
-        object is updated on response.
+        When the thread is started this function is run. It pulls commands from the queue in batches and then sends
+        those commands to elasticsearch.
         """
-        response = self.elasticsearch.bulk(itertools.chain.from_iterable(self.ops))
-        # extract stats from the elasticsearch response
-        for action_response in response['items']:
-            # each item in the items list is a dict with a single key and value, we're interested in the value
-            info = next(iter(action_response.values()))
-            # update the stats
-            self.stats[info['_index']][info['result']] += 1
+        # read commands off the queue and process them in turn
+        for commands in chunk_iterator(iter(self.queue.get, None), chunk_size=self.bulk_size):
+            response = self.elasticsearch.bulk(itertools.chain.from_iterable(commands))
+            # extract stats from the elasticsearch response
+            for action_response in response['items']:
+                # each item in the items list is a dict with a single key and value, we're interested in the value
+                info = next(iter(action_response.values()))
+                # update the stats
+                self.stats[info['_index']][info['result']] += 1
 
 
 class Indexer(Versioned):
@@ -45,19 +54,20 @@ class Indexer(Versioned):
     Class encapsulating the functionality required to index records.
     """
 
-    def __init__(self, version, config, feeder, indexes, elasticsearch_bulk_size=500):
+    def __init__(self, version, config, feeder, indexes, elasticsearch_bulk_size=2000, queue_size=100000):
         """
         :param config: the config object
         :param feeder: feeder object which provides the documents from mongo to inxed
         :param indexes: the indexes that the mongo collection will be indexed into
         :param elasticsearch_bulk_size: the number of pairs of commands to send to elasticsearch in one bulk request
+        :param queue_size: the maximum size of the elasticsearch command queue
         """
         super().__init__(version)
         self.config = config
         self.feeder = feeder
         self.indexes = indexes
         self.elasticsearch_bulk_size = elasticsearch_bulk_size
-
+        self.queue_size = queue_size
         self.elasticsearch = get_elasticsearch_client(self.config, sniff_on_start=True, sniff_on_connection_fail=True,
                                                       sniffer_timeout=60, sniff_timeout=10, http_compress=True)
         self.monitors = []
@@ -103,19 +113,30 @@ class Indexer(Versioned):
         # keep a count of the number of documents indexed so far
         total_indexed_so_far = 0
 
-        with BulkIndexOpBuffer(self.config, self.elasticsearch_bulk_size, self.elasticsearch) as op_buffer:
-            for mongo_doc in self.feeder.documents():
-                total_indexed_so_far += 1
+        # create a queue for elasticsearch commands
+        queue = Queue(maxsize=self.queue_size)
+        # create and then start a thread to send the commands to elasticsearch
+        bulk_writer = ElasticsearchBulkWriterThread(self.elasticsearch, queue, self.elasticsearch_bulk_size)
+        bulk_writer.start()
 
-                for index in self.indexes:
-                    # add each of the commands to the op buffer
-                    for command in index.get_commands(mongo_doc):
-                        op_buffer.add(command)
+        for mongo_doc in self.feeder.documents():
+            total_indexed_so_far += 1
 
-                if total_indexed_so_far % 1000 == 0:
-                    # update the monitoring functions with progress
-                    for monitor in self.monitors:
-                        monitor(total_indexed_so_far / total_records_to_index)
+            for index in self.indexes:
+                # add each of the commands to the op buffer
+                for command in index.get_commands(mongo_doc):
+                    # queue the command, waiting if necessary for the queue to not be full
+                    queue.put(command)
+
+            if total_indexed_so_far % 1000 == 0:
+                # update the monitoring functions with progress
+                for monitor in self.monitors:
+                    monitor(total_indexed_so_far / total_records_to_index)
+
+        # send a sentinel to indicate that we're done putting indexing commands on the queue
+        queue.put(None)
+        # wait for all indexing commands to be sent
+        bulk_writer.join()
 
         # signal to all the monitors that we're done
         for monitor in self.monitors:
@@ -124,7 +145,7 @@ class Indexer(Versioned):
         # update the aliases
         self.update_statuses()
         # report the statistics of the indexing operation back into mongo
-        return self.report_stats(op_buffer.stats)
+        return self.report_stats(bulk_writer.stats)
 
     def define_mappings(self):
         """
