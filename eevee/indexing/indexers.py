80b0e349c7da1a094bf48dd643be57bdf0750d58
Josh Humphries <j.humphries@nhm.ac.uk>
2018-09-07 15:06:55 +0100

    Index from a feeder and index pair
    You can now, in one go, get documents from many feeders and feed them into
    many indexes. However, they must be paired up so that one feeder is matched
    with one index.

--- eevee/indexing/indexers.py
+++ eevee/indexing/indexers.py
@@ -17,16 +17,16 @@ class ElasticsearchBulkWriterThread(Thread):
     Thread which iterates over a queue of elasticsearch indexing commands, sending them off to elasticsearch in batches.
     """
 
-    def __init__(self, indexes, elasticsearch, queue, bulk_size, **kwargs):
+    def __init__(self, index, elasticsearch, queue, bulk_size, **kwargs):
         """
-        :param indexes: the index objects we're indexing into
+        :param index: the index object we're indexing into
         :param elasticsearch: the elasticsearch client object to use
         :param queue: the queue object to take the commands from
         :param bulk_size: how many commands to send to elasticsearch in one request
         :param kwargs: Thread.__init__ kwargs
         """
         super().__init__(**kwargs)
-        self.indexes = indexes
+        self.index = index
         self.elasticsearch = elasticsearch
         self.queue = queue
         self.bulk_size = bulk_size
@@ -42,8 +42,7 @@ class ElasticsearchBulkWriterThread(Thread):
             # change the refresh interval to -1 which means don't refresh at all. This is good for bulk indexing but
             # also means that any changes to the index aren't visible until we reset this value which essentially
             # provides a commit mechanic ensuring all the new data is visible at the same time
-            for index in self.indexes:
-                self.elasticsearch.indices.put_settings({"index": {"refresh_interval": -1}}, index.name)
+            self.elasticsearch.indices.put_settings({"index": {"refresh_interval": -1}}, self.index.name)
 
             # read commands off the queue and process them in turn
             for commands in chunk_iterator(iter(self.queue.get, None), chunk_size=self.bulk_size):
@@ -56,8 +55,7 @@ class ElasticsearchBulkWriterThread(Thread):
                     self.stats[info['_index']][info['result']] += 1
         finally:
             # ensure we put the refresh interval back to the default
-            for index in self.indexes:
-                self.elasticsearch.indices.put_settings({"index": {"refresh_interval": None}}, index.name)
+            self.elasticsearch.indices.put_settings({"index": {"refresh_interval": None}}, self.index.name)
 
 
 class Indexer(Versioned):
@@ -65,18 +63,19 @@ class Indexer(Versioned):
     Class encapsulating the functionality required to index records.
     """
 
-    def __init__(self, version, config, feeder, indexes, elasticsearch_bulk_size=2000, queue_size=100000):
+    def __init__(self, version, config, feeders_and_indexes, elasticsearch_bulk_size=2000, queue_size=100000):
         """
         :param config: the config object
-        :param feeder: feeder object which provides the documents from mongo to inxed
-        :param indexes: the indexes that the mongo collection will be indexed into
+        :param feeders_and_indexes: sequence of 2-tuples where each tuple is made up of a feeder object which provides
+                                    the documents from mongo to index and an index object which will be used to generate
+                                    the data to index from the feeder's documents
         :param elasticsearch_bulk_size: the number of pairs of commands to send to elasticsearch in one bulk request
         :param queue_size: the maximum size of the elasticsearch command queue
         """
         super().__init__(version)
         self.config = config
-        self.feeder = feeder
-        self.indexes = indexes
+        self.feeders_and_indexes = feeders_and_indexes
+        self.feeders, self.indexes = zip(*feeders_and_indexes)
         self.elasticsearch_bulk_size = elasticsearch_bulk_size
         self.queue_size = queue_size
         self.elasticsearch = get_elasticsearch_client(self.config, sniff_on_start=True, sniff_on_connection_fail=True,
@@ -102,7 +101,7 @@ class Indexer(Versioned):
         end = datetime.now()
         stats = {
             'version': self.version,
-            'source': self.feeder.mongo_collection,
+            'source': [feeder.mongo_collection for feeder in self.feeders],
             'start': self.start,
             'end': end,
             'duration': (end - self.start).total_seconds(),
@@ -120,35 +119,38 @@ class Indexer(Versioned):
         self.define_indexes()
 
         # work out the total number of documents we're going to go through and index, for monitoring purposes
-        total_records_to_index = self.feeder.total()
+        total_records_to_index = sum(feeder.total() for feeder in self.feeders)
         # keep a count of the number of documents indexed so far
         total_indexed_so_far = 0
+        # total stats across all feeder/index combinations
+        stats = defaultdict(Counter)
 
-        # create a queue for elasticsearch commands
-        queue = Queue(maxsize=self.queue_size)
-        # create and then start a thread to send the commands to elasticsearch
-        bulk_writer = ElasticsearchBulkWriterThread(self.indexes, self.elasticsearch, queue,
-                                                    self.elasticsearch_bulk_size)
-        try:
-            bulk_writer.start()
-            for mongo_doc in self.feeder.documents():
-                total_indexed_so_far += 1
-
-                for index in self.indexes:
+        for feeder, index in self.feeders_and_indexes:
+            # create a queue for elasticsearch commands
+            queue = Queue(maxsize=self.queue_size)
+            # create and then start a thread to send the commands to elasticsearch
+            bulk_writer = ElasticsearchBulkWriterThread(index, self.elasticsearch, queue, self.elasticsearch_bulk_size)
+            try:
+                bulk_writer.start()
+                for mongo_doc in feeder.documents():
+                    total_indexed_so_far += 1
                     # add each of the commands to the op buffer
                     for command in index.get_commands(mongo_doc):
                         # queue the command, waiting if necessary for the queue to not be full
                         queue.put(command)
 
-                if total_indexed_so_far % 1000 == 0:
-                    # update the monitoring functions with progress
-                    for monitor in self.monitors:
-                        monitor(total_indexed_so_far / total_records_to_index)
-        finally:
-            # send a sentinel to indicate that we're done putting indexing commands on the queue
-            queue.put(None)
-            # wait for all indexing commands to be sent
-            bulk_writer.join()
+                    if total_indexed_so_far % 1000 == 0:
+                        # update the monitoring functions with progress
+                        for monitor in self.monitors:
+                            monitor(total_indexed_so_far / total_records_to_index)
+            finally:
+                # send a sentinel to indicate that we're done putting indexing commands on the queue
+                queue.put(None)
+                # wait for all indexing commands to be sent
+                bulk_writer.join()
+            # update the stats based on the new stats from the bulk writer
+            for index_name, counter in bulk_writer.stats.items():
+                stats[index_name].update(counter)
 
         # signal to all the monitors that we're done
         for monitor in self.monitors:
@@ -157,7 +159,7 @@ class Indexer(Versioned):
         # update the aliases
         self.update_statuses()
         # report the statistics of the indexing operation back into mongo
-        return self.report_stats(bulk_writer.stats)
+        return self.report_stats(stats)
 
     def define_indexes(self):
         """
