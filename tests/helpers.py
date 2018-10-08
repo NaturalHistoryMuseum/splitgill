2624e2872723e7ef8cd2f63d87b7d4688f093652
Josh Humphries <j.humphries@nhm.ac.uk>
2018-10-08 23:10:10 +0100

    Remove Bunch, add mock lib, use
    Don't need a Bunch class when we can use the Mock and MagicMock classes
    from the mock lib. The mock lib will be useful later anyway too.

--- tests/helpers.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# encoding: utf-8
-
-
-# h/t http://code.activestate.com/recipes/52308-the-simple-but-handy-collector-of-a-bunch-of-named/
-class Bunch(object):
-    def __init__(self, **kwds):
-        self.__dict__.update(kwds)
--- tests/test_mongo.py
+++ tests/test_mongo.py
@@ -1,32 +1,30 @@
 #!/usr/bin/env python3
 # encoding: utf-8
-
+from mock import MagicMock
 from pymongo.collection import Collection
 from pymongo.database import Database
 
 from eevee.mongo import get_mongo
-from tests.helpers import Bunch
 
 from pymongo import MongoClient
 
 
 class TestMongo(object):
-
-    def setup(self):
-        self.config = Bunch(mongo_host='localhost', mongo_port=27017, mongo_database='test_database')
+    # note that these tests use the actual pymongo lib but don't connect to any databases (the clients are lazy)
+    config = MagicMock(mongo_host='localhost', mongo_port=27017, mongo_database='test_database')
 
     def test_get_with_just_config(self):
-        with get_mongo(self.config) as mongo:
+        with get_mongo(TestMongo.config) as mongo:
             assert type(mongo) is MongoClient
 
     def test_get_with_config_and_database(self):
-        with get_mongo(self.config, database='test_database') as mongo:
+        with get_mongo(TestMongo.config, database='test_database') as mongo:
             assert type(mongo) is Database
 
     def test_get_with_config_and_collection(self):
-        with get_mongo(self.config, collection='test_collection') as mongo:
+        with get_mongo(TestMongo.config, collection='test_collection') as mongo:
             assert type(mongo) is Collection
 
     def test_get_with_config_and_database_and_collection(self):
-        with get_mongo(self.config, database='test_database', collection='test_collection') as mongo:
+        with get_mongo(TestMongo.config, database='test_database', collection='test_collection') as mongo:
             assert type(mongo) is Collection
