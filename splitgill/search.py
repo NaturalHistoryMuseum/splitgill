0860111d3eef42ec8221b516aefa0886421c6e0b
Ginger Burns <alycejenni@gmail.com>
2025-12-29 17:15:47 +0000

    style: automated formatting (except quotes)

--- splitgill/search.py
+++ splitgill/search.py
@@ -247,7 +247,7 @@ def rebuild_data(parsed_data: dict) -> dict:
 
 
 def rebuild_dict_or_list(
-    value: Union[dict, list]
+    value: Union[dict, list],
 ) -> Union[int, str, bool, float, dict, list, None]:
     """
     Rebuild a dict or a list inside the parsed dict.
